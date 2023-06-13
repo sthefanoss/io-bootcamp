e45dd9a7b4ad2a40754ccb7def28e54ae874c99f
sthefano <sthefanoss@gmail.com>
2023-06-13 16:11:02 -0300

    feat: add features to xylophone app

--- 07 - Xylophone/Xylophone/ViewController.swift
+++ 07 - Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        changeOpacity(button: sender)
+        playSound(buttonTitle: sender.currentTitle!)
+   
+    }
+    
+    func changeOpacity(button: UIButton) {
+        button.layer.opacity = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            button.layer.opacity = 1
+        }
+    }
+    
+    func playSound(buttonTitle : String) {
+        let url = Bundle.main.url(forResource: buttonTitle, withExtension: "wav" )!
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+    }
+    
 }
 
