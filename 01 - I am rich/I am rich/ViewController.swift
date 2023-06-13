9e6f3655490a2648acd10f74a9dbc3a9b8b7cdca
sthefano <s.schiavon_ext@teladochealth.com>
2023-06-12 21:42:34 -0300

    feat: upload first app

---
+++ 01 - I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Sthefano Schiavon on 12/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
